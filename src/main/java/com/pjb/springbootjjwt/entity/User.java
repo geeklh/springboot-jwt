54a841d4989a712d03933ca7c21c29a975d924b6
geeklh <1033797938@qq.com>
2019-11-15 17:35:02 +0800

    ff

--- src/main/java/com/pjb/springbootjjwt/entity/User.java
+++ src/main/java/com/pjb/springbootjjwt/entity/User.java
@@ -15,4 +15,28 @@ public class User {
     String Id;
     String username;
     String password;
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String id) {
+        Id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
